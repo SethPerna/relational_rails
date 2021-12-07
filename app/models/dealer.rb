bf7658be72a47e6da510a15e803d6f03f3f46c2b
craig cardwell <wizeduvcc@gmail.com>
2021-12-06 18:23:51 -0700

    feat: add user story 6 descending order index for dealer with created at date

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -4,4 +4,8 @@ class Dealer < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :open
   validates_presence_of :zip
+
+  def self.in_order
+    Dealer.order(created_at: :desc)
+  end
 end
